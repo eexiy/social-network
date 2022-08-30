dec4e7afaefd280a50cecbff84048673980f39cd
Ali Suiarov <alizhansuiarov@gmail.com>
2022-08-30 20:42:53 +0300

    thirty-first lesson

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post';
 
@@ -5,16 +6,23 @@ const MyPosts = (props) => {
 
     const postsElement = props.postsData.map(post => <Post message={post.post} likes={post.likes} />)
 
+    const newPostElement = React.useRef()
+
+    const addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text)
+    }
+
     return (
         <div className={s.content}>
             <div>
                 <h3>My Posts</h3>
                 <div>
                     <div>
-                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <textarea name="" id="" cols="30" rows="3" ref={newPostElement}></textarea>
                     </div>
                     <div>
-                        <button>Add post</button>
+                        <button onClick={addPost}>Add post</button>
                     </div>
                 </div>
                 <div className={s.posts}>
