ce54a528cc6610616e816dba0afca9224b4b6713
Ali Suiarov <alizhansuiarov@gmail.com>
2022-09-20 22:49:25 +0300

    fifty-fourth lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,13 +5,10 @@ import axios from 'axios'
 
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props)
-        alert('ds')
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(response.data.items)
-        })
+        }) 
     }
 
     render() {
